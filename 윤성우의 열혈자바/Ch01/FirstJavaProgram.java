148601f607d046a3f3c2d3466bd52c1815362644
윤동훈 <do81818@naver.com>
2021-08-25 20:58:10 +0900

    init

--- 윤성우의 열혈자바/Ch01/FirstJavaProgram.java
+++
@@ -1,8 +0,0 @@
-package Ch01;
-
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("First Java program");
-    }
-}
